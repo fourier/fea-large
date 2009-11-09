852446591e784580506d4653816ef626ad1f6c82
fourier <fourier@6667f7cb-50dd-46f4-b9ff-056280f8eac2>
2009-11-09 16:40:11 +0000

    Updated, started implementation of the XML parser

--- c/fea_solver.c
+++ c/fea_solver.c
@@ -6,6 +6,10 @@
 #endif
 
 /*************************************************************/
+/* Constants                                                 */
+#define READ_BUF_SIZE 10000
+
+/*************************************************************/
 /* Type definitions                                          */
 
 /* BOOL type as usual */
@@ -99,15 +103,6 @@ typedef struct prescribed_boundary_array_tag {
   prescibed_boundary_node *prescribed_nodes;
 } prescribed_boundary_array;
 
-/* An input data structure used in parser */
-typedef struct input_data_tag {
-  fea_task *task;
-  fea_solution_params *fea_params;
-  nodes_array *nodes;
-  elements_array *elements;
-  prescribed_boundary_array *presc_boundary;
-} input_data;
-
 
 /*************************************************************/
 /* Application-specific structures                           */
@@ -141,21 +136,17 @@ typedef struct elements_database_tag {
 /*************************************************************/
 /* Functions declarations                                    */
 
-/*
- * Initialize global data
- */
-void initialize_globals(void);
-
-/*
- * Clear global data
- */
-void clear_globals(void);
-
 
 /*
  * Load initial data from file
  */
-BOOL initial_data_load(char *filename);
+BOOL initial_data_load(char *filename,
+                       fea_task **task,
+                       fea_solution_params **fea_params,
+                       nodes_array **nodes,
+                       elements_array **elements,
+                       prescribed_boundary_array **presc_boundary);
+
  
 
 /* function for calculation value of shape function for 10-noded
@@ -186,26 +177,50 @@ real df_dt(int i, real r, real s, real t);
 
 /*************************************************************/
 /* Global variables                                          */
-input_data* g_input_data;
-elements_database *g_element_db;
 
-int main(int argc, char **argv)
+
+int parse_cmdargs(int argc, char **argv,char **filename)
 {
-  atexit(&clear_globals);
-  initialize_globals();
   if (argc < 2)
   {
     printf("Usage: fea_solve input_data.xml\n");
     return 1;
   }
-  if(!initial_data_load(argv[1]))
-  {
-    printf("Error. Unable to load %s.\n",argv[1]);
-    return 1;
-  }
-  
-
+  *filename = argv[1];
   return 0;
+}
+
+int do_main(char* filename)
+{
+  int result = 0;
+  fea_task *task;
+  fea_solution_params *fea_params;
+  nodes_array *nodes;
+  elements_array *elements;
+  prescribed_boundary_array *presc_boundary;
+  
+  if(!initial_data_load(filename,&task,&fea_params,&nodes,&elements,&presc_boundary))
+  {
+    printf("Error. Unable to load %s.\n",filename);
+    result = 1;
+  }
+  return result;
+}
+
+int main(int argc, char **argv)
+{
+  char* filename = 0;
+  int result = 0;
+  
+  do
+  {
+    if ( result = parse_cmdargs(argc, argv,&filename))
+      break;
+    if ( result = do_main(filename))
+      break;
+  } while(0);
+
+  return result;
 }
 
 
@@ -313,6 +328,12 @@ static fea_task* initialize_fea_task()
   return task;
 }
 
+static void free_fea_task(fea_task* task)
+{
+  /* TODO: implement this */
+
+}
+
 /* Initializes fea solution params with default values */
 static fea_solution_params* initialize_fea_solution_params()
 {
@@ -326,6 +347,7 @@ static fea_solution_params* initialize_fea_solution_params()
   return fea_params;
 }
 
+
 /* Initialize nodes array but not initialize particular arrays  */
 static nodes_array* initialize_nodes_array()
 {
@@ -337,6 +359,12 @@ static nodes_array* initialize_nodes_array()
   return nodes;
 }
 
+static void free_nodes_array(nodes_array* array)
+{
+  /* TODO: implement this */
+}
+
+
 /* Initialize elements array but not initialize particular elements */
 static elements_array* initialize_elements_array()
 {
@@ -346,6 +374,11 @@ static elements_array* initialize_elements_array()
   elements->elements = (void*)0;
   elements->elements_count = 0;
   return elements;
+}
+
+static free_elements_array(elements_array *elements)
+{
+  /* TODO: implement this */
 }
 
 /* Initialize boundary nodes array but not initialize particular nodes */
@@ -360,9 +393,16 @@ static prescribed_boundary_array* initialize_prescribed_boundary_array()
   return presc_boundary;
 }
 
+static free_prescribed_boundary_array(prescribed_boundary_array* presc)
+{
+  /* TODO: implement this */
+}
+
+
 #ifdef USE_EXPAT
 
 typedef enum xml_format_tags_enum {
+  UNKNOWN_TAG,
   TASK,
   MODEL,
   MODEL_TYPE,
@@ -400,62 +440,162 @@ typedef enum xml_format_tags_enum {
   PRESC_TYPE,
 } xml_format_tags;
 
+enum xml_format_tags tagname_to_enum(const XML_Char* name)
+{
+  if (!strcmp(name,"task")) return TASK;
+  if (!strcmp(name,"model")) return MODEL;
+  if (!strcmp(name,"model_type")) return MODEL_TYPE;
+  if (!strcmp(name,"model_parameters")) return MODEL_PARAMETERS;
+  if (!strcmp(name,"lambda")) return LAMBDA;
+  if (!strcmp(name,"mu")) return MU;
+  if (!strcmp(name,"solution")) return SOLUTION;
+  if (!strcmp(name,"modified_newton")) return MODIFIED_NEWTON;
+  if (!strcmp(name,"task_type")) return TASK_TYPE;
+  if (!strcmp(name,"element_type")) return ELEMENT_TYPE;
+  if (!strcmp(name,"gauss_nodes_count")) return GAUSS_NODES_COUNT;
+  if (!strcmp(name,"load_increments_count")) return LOAD_INCREMENTS_COUNT;
+  if (!strcmp(name,"desired_tolerance")) return DESIRED_TOLERANCE;
+  if (!strcmp(name,"line_search")) return LINE_SEARCH;
+  if (!strcmp(name,"line_search_max")) return LINE_SEARCH_MAX;
+  if (!strcmp(name,"arc_length")) return ARC_LENGTH;
+  if (!strcmp(name,"arc_length_max")) return ARC_LENGTH_MAX;
+  if (!strcmp(name,"input_data")) return INPUT_DATA;
+  if (!strcmp(name,"geometry")) return GEOMETRY;
+  if (!strcmp(name,"nodes")) return NODES;
+  if (!strcmp(name,"node")) return NODE;
+  if (!strcmp(name,"x")) return X;
+  if (!strcmp(name,"y")) return Y;
+  if (!strcmp(name,"z")) return Z;
+  if (!strcmp(name,"elements")) return ELEMENTS;
+  if (!strcmp(name,"element")) return ELEMENT;
+  if (!strcmp(name,"node_id")) return NODE_ID;
+  if (!strcmp(name,"boundary_conditions")) return BOUNDARY_CONDITIONS;
+  if (!strcmp(name,"prescribed_displacements")) return PRESCRIBED_DISPLACEMENTS;
+  if (!strcmp(name,"presc_node")) return PRESC_NODE;
+  if (!strcmp(name,"presc_id")) return PRESC_ID;
+  if (!strcmp(name,"presc_x")) return PRESC_X;
+  if (!strcmp(name,"presc_y")) return PRESC_Y;
+  if (!strcmp(name,"presc_z")) return PRESC_Z;
+  if (!strcmp(name,"presc_type")) return PRESC_TYPE;
+  return UNKNOWN_TAG;
+}
+
+/* An input data structure used in parser */
+typedef struct parse_data_tag {
+  fea_task *task;
+  fea_solution_params *fea_params;
+  nodes_array *nodes;
+  elements_array *elements;
+  prescribed_boundary_array *presc_boundary;
+  xml_format_tags last_tag;
+  char* current_text;
+  int current_size;
+} parse_data;
+
+
 void expat_start_tag_handler(void *userData,
                       const XML_Char *name,
                       const XML_Char **atts)
 {
+  parse_data* data = (parse_data*)userData;
+  data->last_tag = tagname_to_enum(name);
   printf("open: %s\n",name);
 }
 
 void expat_end_tag_handler(void *userData,
                    const XML_Char *name)
 {
-  printf("%close s\n",name);
+  parse_data* data = (parse_data*)userData;
+  printf("total text len = %d\n",data->current_size);
+  data->current_text = (char*)0;
+  data->current_size = 0;
+  printf("close %s\n",name);
 }
 
 void expat_text_handler(void *userData,
                         const XML_Char *s,
                         int len)
 {
-  printf("text len = %d\n",len);
+  parse_data* data = (parse_data*)userData;
+  char *ptr;
+  if (!data->current_text)
+  {
+    data->current_text = (char*)malloc(len);
+    ptr = data->current_text;
+  }
+  else
+  {
+    /* FIXME: after every reallocation data->current_text is another pointer!!!*/
+    ptr = data->current_text;
+    data->current_text = (char*)realloc(data->current_text,data->current_size+len);
+    ptr += data->current_size;  
+  }
+  memcpy(ptr,s,len);
+  data->current_size += len;
+  printf("fragment text len = %d\n",len);
 }
 
 
-static BOOL expat_data_load(char *filename)
+static BOOL expat_data_load(char *filename,
+                            fea_task **task,
+                            fea_solution_params **fea_params,
+                            nodes_array **nodes,
+                            elements_array **elements,
+                            prescribed_boundary_array **presc_boundary)
 {
   BOOL result = FALSE;
-  FILE* xmlDocumentFile;
+  FILE* xml_document_file;
   XML_Parser parser;
-  long int fileSize = 0;
+  size_t file_size = 0;
+  size_t read_bytes = 0;
+  parse_data parse;
+  enum XML_Status status;
+  char *file_contents = (char*)0;
+  char *file_current_position = file_contents;
+  
   /* Try to open file */
-  if (!(xmlDocumentFile = fopen(filename,"rt")))
+  if (!(xml_document_file = fopen(filename,"rt")))
   {
     printf("Error, could not open file %s\n",filename);
     return FALSE;
   }
   /* Determine file size */
-  if (fseek(xmlDocumentFile,0,SEEK_END))
+  if (fseek(xml_document_file,0,SEEK_END))
   {
     printf("Error reading file %s\n",filename);
     return FALSE;
   }
-  fileSize = ftell(xmlDocumentFile);
+  file_size = ftell(xml_document_file);
   /* rewind to the begin of file */
-  fseek(xmlDocumentFile,0,SEEK_SET);
+  fseek(xml_document_file,0,SEEK_SET);
 
   /* Create parser */
   parser = XML_ParserCreate(NULL);
   /* Set handlers */
   XML_SetElementHandler(parser, &expat_start_tag_handler, &expat_end_tag_handler);
   XML_SetCharacterDataHandler(parser,expat_text_handler);
-  
-  /* *task = initialize_fea_task(); */
-  /* *input = (input_data*)malloc(input); */
+
+  /* initialize data */
+  parse.current_text = (char*)0;
+  parse.current_size = 0;
+  /* TODO: add initialization here */
+  XML_SetUserData(parser,&parse);
+
+  /* read whole file */
+  file_contents = (char*)malloc(file_size);
+  read_bytes = fread(file_contents,1,file_size,xml_document_file);
+  if (errno)
+  {
+    free(file_contents);
+    /* deallocate parse data */
+    return FALSE;
+  }
+  /* call parser */
+  status = XML_Parse(parser,file_contents,(int)read_bytes,1);
+  free(file_contents);
   
   return result;
 }
-
-
 
 
 
@@ -463,24 +603,18 @@ static BOOL expat_data_load(char *filename)
 
 
 
-BOOL initial_data_load(char *filename)
+BOOL initial_data_load(char *filename,
+                       fea_task **task,
+                       fea_solution_params **fea_params,
+                       nodes_array **nodes,
+                       elements_array **elements,
+                       prescribed_boundary_array **presc_boundary)
 {
   BOOL result = FALSE;
 #ifdef USE_EXPAT
-  result = expat_data_load(filename);
+  result = expat_data_load(filename,task,fea_params,nodes,elements,presc_boundary);
 #endif
   
   return result;
 }
-
-
-void initialize_globals(void)
-{
-  
-}
-
-void clear_globals(void)
-{
-}
-
 
