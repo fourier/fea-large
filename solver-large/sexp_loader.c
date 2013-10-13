ec7cc7ce1794f2ceec073938c58c5574690b4909
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2013-10-13 13:37:05 +0200

    Solver update to latest libsexp

--- solver-large/sexp_loader.c
+++ solver-large/sexp_loader.c
@@ -35,19 +35,19 @@ static void process_model(sexp_item* item, parse_data* data)
   sexp_item* value = sexp_item_attribute(item,"name");
   if (value)
   {
-    if (sexp_item_is_symbol(value,"A5"))
+    if (sexp_item_is_symbol_like(value,"A5"))
     {
       data->task->model.model = MODEL_A5;
       data->task->model.parameters_count = 2;
     }
-    else if (sexp_item_is_symbol(value,"COMPRESSIBLE_NEOHOOKEAN"))
+    else if (sexp_item_is_symbol_like(value,"COMPRESSIBLE_NEOHOOKEAN"))
     {
       data->task->model.model = MODEL_COMPRESSIBLE_NEOHOOKEAN;
       data->task->model.parameters_count = 2;
     }
     else
     {
-      printf("unknown model type '%s'\n",value->atom->value.symbol);
+      printf("unknown model type '%s'\n",sexp_item_symbol(value));
     }
   }
 }
@@ -61,10 +61,10 @@ static void process_model_parameters(sexp_item* item, parse_data* data)
   case MODEL_A5:
     value = sexp_item_attribute(item,"lambda");
     assert(value);
-    data->task->model.parameters[0] = atom_token_fnumber(value->atom);
+    data->task->model.parameters[0] = sexp_item_fnumber(value);
     value = sexp_item_attribute(item,"mu");
     assert(value);
-    data->task->model.parameters[1] = atom_token_fnumber(value->atom);
+    data->task->model.parameters[1] = sexp_item_fnumber(value);
     break;
   default:
     break;
@@ -76,21 +76,21 @@ static void process_solution(sexp_item* item, parse_data* data)
   sexp_item* value;
   value = sexp_item_attribute(item,"desired-tolerance");
   assert(value);
-  data->task->desired_tolerance = atom_token_fnumber(value->atom);
+  data->task->desired_tolerance = sexp_item_fnumber(value);
   value = sexp_item_attribute(item,"task-type");
   assert(value);
-  if (sexp_item_is_symbol(value,"CARTESIAN3D"))
+  if (sexp_item_is_symbol_like(value,"CARTESIAN3D"))
     data->task->type = CARTESIAN3D;
   value = sexp_item_attribute(item,"load-increments-count");
   assert(value);
-  data->task->load_increments_count = atom_token_inumber(value->atom);
+  data->task->load_increments_count = sexp_item_inumber(value);
   value = sexp_item_attribute(item,"modified-newton");
   assert(value);
   data->task->modified_newton = FALSE;
-  if (sexp_item_is_symbol(value,"YES") || sexp_item_is_symbol(value,"TRUE"))
+  if (sexp_item_is_symbol_like(value,"YES") || sexp_item_is_symbol_like(value,"TRUE"))
     data->task->modified_newton = TRUE;
   value = sexp_item_attribute(item,"max-newton-count");
-  data->task->max_newton_count = atom_token_inumber(value->atom);
+  data->task->max_newton_count = sexp_item_inumber(value);
 }
 
 static void process_slae_solver(sexp_item* item, parse_data* data)
@@ -103,33 +103,33 @@ static void process_slae_solver(sexp_item* item, parse_data* data)
   data->task->solver_max_iter = MAX_ITERATIVE_ITERATIONS;
   if (value)
   {
-    if (sexp_item_is_symbol(value,"CG"))
+    if (sexp_item_is_symbol_like(value,"CG"))
     {
       data->task->solver_type = CG;
       value = sexp_item_attribute(item,"tolerance");
       if (value)
-        data->task->solver_tolerance = atom_token_fnumber(value->atom);
+        data->task->solver_tolerance = sexp_item_fnumber(value);
       value = sexp_item_attribute(item,"max-iterations");
-      data->task->solver_max_iter = value ? atom_token_inumber(value->atom) :
+      data->task->solver_max_iter = value ? sexp_item_inumber(value) :
         MAX_ITERATIVE_ITERATIONS;
     }
-    else if (sexp_item_is_symbol(value,"PCG_ILU"))
+    else if (sexp_item_is_symbol_like(value,"PCG_ILU"))
     {
       data->task->solver_type = PCG_ILU;
       value = sexp_item_attribute(item,"tolerance");
       if (value)
-        data->task->solver_tolerance = atom_token_fnumber(value->atom);
+        data->task->solver_tolerance = sexp_item_fnumber(value);
       value = sexp_item_attribute(item,"max-iterations");
-      data->task->solver_max_iter = value ? atom_token_inumber(value->atom) :
+      data->task->solver_max_iter = value ? sexp_item_inumber(value) :
         MAX_ITERATIVE_ITERATIONS;
     } 
-    else if (sexp_item_is_symbol(value,"CHOLESKY"))
+    else if (sexp_item_is_symbol_like(value,"CHOLESKY"))
     {
       data->task->solver_type = CHOLESKY;
     } 
     else
     {
-      printf("unknown solver type '%s'\n",value->atom->value.symbol);
+      printf("unknown solver type '%s'\n",sexp_item_symbol(value));
     }
   }
 }
@@ -140,13 +140,13 @@ static void process_element_type(sexp_item* item, parse_data* data)
   sexp_item* value;
   value = sexp_item_attribute(item,"gauss-nodes-count");
   assert(value);
-  data->fea_params->gauss_nodes_count = atom_token_inumber(value->atom);
+  data->fea_params->gauss_nodes_count = sexp_item_inumber(value);
   value = sexp_item_attribute(item,"nodes-count");
   assert(value);
-  data->fea_params->nodes_per_element = atom_token_inumber(value->atom);
+  data->fea_params->nodes_per_element = sexp_item_inumber(value);
   value = sexp_item_attribute(item,"name");
   assert(value);
-  if (sexp_item_is_symbol(value,"TETRAHEDRA10"))
+  if (sexp_item_is_symbol_like(value,"TETRAHEDRA10"))
     data->task->ele_type = TETRAHEDRA10;
 }
 
@@ -155,7 +155,7 @@ static void process_line_search(sexp_item* item, parse_data* data)
   sexp_item* value;
   value = sexp_item_attribute(item,"max");
   assert(value);
-  data->task->linesearch_max = atom_token_inumber(value->atom);
+  data->task->linesearch_max = sexp_item_inumber(value);
 }
 
 static void process_arc_length(sexp_item* item, parse_data* data)
@@ -163,7 +163,7 @@ static void process_arc_length(sexp_item* item, parse_data* data)
   sexp_item* value;
   value = sexp_item_attribute(item,"max");
   assert(value);
-  data->task->arclength_max = atom_token_inumber(value->atom);
+  data->task->arclength_max = sexp_item_inumber(value);
 }
 
 static void process_nodes(sexp_item* item, parse_data* data)
@@ -181,9 +181,9 @@ static void process_nodes(sexp_item* item, parse_data* data)
     data->nodes->nodes[i] = (real*)malloc(MAX_DOF*sizeof(real));
     item = sexp_item_car(next);
     assert(sexp_item_length(item) == 3);
-    data->nodes->nodes[i][0] = atom_token_fnumber(sexp_item_nth(item,0)->atom);
-    data->nodes->nodes[i][1] = atom_token_fnumber(sexp_item_nth(item,1)->atom);
-    data->nodes->nodes[i][2] = atom_token_fnumber(sexp_item_nth(item,2)->atom);
+    data->nodes->nodes[i][0] = sexp_item_fnumber(sexp_item_nth(item,0));
+    data->nodes->nodes[i][1] = sexp_item_fnumber(sexp_item_nth(item,1));
+    data->nodes->nodes[i][2] = sexp_item_fnumber(sexp_item_nth(item,2));
     next = sexp_item_cdr(next);
   }
 }
@@ -206,7 +206,7 @@ static void process_elements(sexp_item* item, parse_data* data)
     assert(sexp_item_length(item) == data->fea_params->nodes_per_element);
     for ( j = 0; j < data->fea_params->nodes_per_element; ++ j)
       data->elements->elements[i][j] =
-        atom_token_inumber(sexp_item_nth(item,j)->atom);
+        sexp_item_inumber(sexp_item_nth(item,j));
     next = sexp_item_cdr(next);
   }
 }
@@ -230,16 +230,16 @@ static void process_prescribed(sexp_item* item, parse_data* data)
 
     assert(sexp_item_starts_with_symbol(item,"presc-node"));
     data->presc_boundary->prescribed_nodes[i].node_number =
-      atom_token_inumber(sexp_item_attribute(item,"node-id")->atom);
+      sexp_item_inumber(sexp_item_attribute(item,"node-id"));
     data->presc_boundary->prescribed_nodes[i].values[0] =
-      atom_token_fnumber(sexp_item_attribute(item,"x")->atom);
+      sexp_item_fnumber(sexp_item_attribute(item,"x"));
     data->presc_boundary->prescribed_nodes[i].values[1] =
-      atom_token_fnumber(sexp_item_attribute(item,"y")->atom);
+      sexp_item_fnumber(sexp_item_attribute(item,"y"));
     data->presc_boundary->prescribed_nodes[i].values[2] =
-      atom_token_fnumber(sexp_item_attribute(item,"z")->atom);
+      sexp_item_fnumber(sexp_item_attribute(item,"z"));
     data->presc_boundary->prescribed_nodes[i].type =
       (presc_boundary_type)
-      atom_token_inumber(sexp_item_attribute(item,"type")->atom);
+      sexp_item_inumber(sexp_item_attribute(item,"type"));
 
     next = sexp_item_cdr(next);
   }
